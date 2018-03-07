cdf5c02e4e82b72bdb62cb6735dfa37f2c2e44aa
younerd4ever <37155566+younerd4ever@users.noreply.github.com>
2018-03-07 20:32:54 +0200

    Add files via upload

---
+++ who are u by eyes and years old.py
@@ -0,0 +1,61 @@
+
+
+yars = int(input("How old are u? "))
+
+bl = 'blue'
+br = 'brown'
+gr = 'green'
+rd = 'red'
+
+eye = [bl, br, gr, rd]
+
+
+a = str(input("what color of eyes do u have?(green, blue, brown or red) "))
+
+
+if a == bl and (yars > 0 and yars < 5 ) :
+    print('Child')
+elif a == br and (yars > 0 and yars < 5 ) :
+    print('Child')
+elif a == gr and (yars > 0 and yars < 5 ) :
+    print('Child')
+elif a == rd and (yars > 0 and yars < 5 ) :
+    print('Stupid')
+
+
+
+elif a == br and (yars > 5 and yars < 10 ) :
+    print('Kid')
+elif a == bl and (yars > 5 and yars < 10 ) :
+    print('Kid')
+elif a == gr and (yars > 5 and yars < 10 ) :
+    print('Kid')
+elif a == rd and (yars > 5 and yars < 10 ) :
+    print('Alien')
+
+
+
+elif a == br and (yars > 10 and yars < 17 ) :
+    print('Teen')
+elif a == bl and (yars > 10 and yars < 17 ) :
+    print('Teen')
+elif a == gr and (yars > 10 and yars < 17 ) :
+    print('Teen')
+elif a == rd and (yars > 10 and yars < 17 ) :
+    print('No comments')
+
+
+
+elif a == br and (yars > 17 and yars < 999999999999 ) :
+    print('Full-fledged person')
+elif a == br and (yars > 17 and yars < 999999999999 ) :
+    print('Full-fledged person')
+elif a == br and (yars > 17 and yars < 999999999999 ) :
+    print('Full-fledged person')
+elif a == rd and (yars > 17 and yars < 999999999999 ) :
+    print('I cant say who are u')
+
+# If u like this project - please subscribe my Instagram: @y.o.u.n.e..r.d With grate wishes
+
+
+
